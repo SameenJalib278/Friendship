d0b21bc3cbc64850c6e4f19f547a51700ca79ef0
John Doe <johndoe@example.com>
2021-12-29 21:11:56 -0800

    commit

--- src/component/todoreact/Todo.js
+++ src/component/todoreact/Todo.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './style.css';
 
-const getLocalData=()=>{
-    const list= localStorage.getItem("mytodo");
-    if(list){
+const getLocalData = () => {
+    const list = localStorage.getItem("mytodo");
+    if (list) {
         return JSON.parse(list);
-    }else{
+    } else {
         return [];
     }
 }
@@ -15,99 +15,136 @@ const Todo = () => {
     const [item, setItem] = useState(getLocalData());
     const [isEditItem, setIsEditItem] = useState("");
     const [togglButton, settoggleButton] = useState(false);
+    const [Source, setSource1] = useState("");
+    const [Destination, setDestination] = useState("");
 
-    const addItem = () =>{
-        if(!inputdata)
-    {
-        alert ("please fill the data");
-    } else if(inputdata && togglButton){
-        setItem(
-            item.map((curElem)=>{
-               if(curElem.id === isEditItem){
-                   return {...curElem, name:inputdata}
-               }
-               return curElem;
-            })
-        )
-        setInputdata("");
-        setIsEditItem(null);
-        settoggleButton(false);
-    } 
-    
-    else{
-        const myNewInputData ={
-            id: new Date().getTime().toString(),
-            name: inputdata,
-        };
-        setItem([...item, myNewInputData]);
-        setInputdata("");
-    }
-};
-        
-        const deleteItem =(index)=>{
-            const updatedItem = item.filter((curElem)=>{
-            return curElem.id !== index;
-            }
-            );
-            setItem(updatedItem);
-        };
-      const removeAll= ()=>{
-          setItem([]);
-      };
-
-      useEffect(()=>{
-
-      localStorage.setItem("mytodo", JSON.stringify(item));
+    const addSource= (Source)=>{
+        if(Source){
           
-      }, [item])
+          alert("person1 added successfully");
+          setSource1(Source);
+        }
+    }
 
-      const editItem = (index)=>{
-          const item_todo_edited = item.find((curElem)=>{
-              return curElem.id ===index;
-          }
-          );
-          setInputdata(item_todo_edited.name);
-          setIsEditItem(index);
-          settoggleButton(true);
-      }
+    const addDestination=(Destination)=>{
+        if(Destination){
+            alert("Person2 Added Successfully");
+            setDestination(Destination);
+        }
+    }
+
+    const addItem = () => {
+        if (!inputdata) {
+            alert("please fill the data");
+        } else if (inputdata && togglButton) {
+            setItem(
+                item.map((curElem) => {
+                    if (curElem.id === isEditItem) {
+                        return { ...curElem, name: inputdata }
+                    }
+                    return curElem;
+                })
+            )
+            setInputdata("");
+            setIsEditItem(null);
+            settoggleButton(false);
+        }
+
+        else {
+            const myNewInputData = {
+                id: new Date().getTime().toString(),
+                name: inputdata,
+            };
+            setItem([...item, myNewInputData]);
+            setInputdata("");
+        }
+    };
+
+    const deleteItem = (index) => {
+        const updatedItem = item.filter((curElem) => {
+            return curElem.id !== index;
+        }
+        );
+        setItem(updatedItem);
+    };
+    const removeAll = () => {
+        setItem([]);
+    };
+
+    useEffect(() => {
+
+        localStorage.setItem("mytodo", JSON.stringify(item));
+
+    }, [item])
+
+    const editItem = (index) => {
+        const item_todo_edited = item.find((curElem) => {
+            return curElem.id === index;
+        }
+        );
+        setInputdata(item_todo_edited.name);
+        setIsEditItem(index);
+        settoggleButton(true);
+    }
 
     return (
         <>
             <div className='main-div'>
                 <div className='child-div'>
                     <figure>
-                        <img src='../images/todo.svg' alt="todologo" />
+                        <img src='../images/R.png' alt="todologo" />
               <figcaption>Add Your List Here ✌ </figcaption>
              </figure>
-             <div className='addItems'>
-<input type="text" placeholder='✍ Add Item' className='form-control' value={inputdata} onChange={(event)=>setInputdata(event.target.value)} />
-             { 
-                togglButton?(
-             <i class="fa fa-edit add-btn" onClick={addItem} ></i>):
-            ( <i class="fa fa-plus add-btn" onClick={addItem} ></i>)
-                }
+                    <div className='addItems'>
+                        <input type="text" placeholder='✍ Add Item' className='form-control' value={inputdata} onChange={(event) => setInputdata(event.target.value)} />
+                        {
+                            togglButton ? (
+                                <i class="fa fa-edit add-btn" onClick={addItem} ></i>) :
+                                (<i class="fa fa-plus add-btn" onClick={addItem} ></i>)
+                        }
 
-              </div>
+                    </div>
 
-              <div className='showItems'>
-                  { item.map((curElem)=>{
-                      return(
-                        <div className='eachItem' key={curElem.id}>
-                        <h3>{curElem.name}</h3>
-                        <div className='todo-btn'>
-                            <i className="far fa-edit add btn" onClick={()=>editItem(curElem.id)}></i>
-                            <i className="far fa-trash-alt  add btn" onClick={()=>deleteItem(curElem.id)}></i>
+                    <div className='showItems'>
+                        {item.map((curElem) => {
+                            return (
+                                <div className='eachItem' key={curElem.id}>
+                                    <h3>{curElem.name}</h3>
+                                    <div className='todo-btn'>
+                                        <i className="far fa-edit add btn" onClick={() => editItem(curElem.id)}></i>
+                                        <i className="far fa-trash-alt  add btn" onClick={() => deleteItem(curElem.id)}></i>
+                                    </div>
+                                </div>
+                            );
+                        })}</div>
+
+
+
+                    <div className='showItems'>
+                        <div class="dropdown">
+                            <button class="dropbtn">Person1</button>
+                            <div class="dropdown-content">
+                              <button onClick={addSource} value={Source}>Sameen</button><br></br>
+                              <button onClick={addSource} value={Source}>Shahzad</button><br></br>
+                              <button onClick={addSource} value={Source}>Sameer</button>
+                            </div>
                         </div>
-                        </div>
-                      );
-                  })}</div>
 
-            <div className='showItems'>
-                <button className='btn effect04' data-sm-link-text="Remove All" onClick={removeAll}>
-                    <span >check list</span>
-                </button>
-            </div>
-            </div>
+                        <div class="dropdown">
+                            <button class="dropbtn">Person2</button>
+                            <div class="dropdown-content">
+                            <button onClick={addDestination} value={Destination}>Sameen</button><br></br>
+                              <button onClick={addDestination} value={Destination}>Shahzad</button><br></br>
+                              <button onClick={addDestination} value={Destination}>Sameer</button>
+                            </div>
+                        </div>
+
+
+                        <button className='btn effect04' data-sm-link-text="View Relation" onClick={removeAll}>
+                            <span >View Relation</span>
+                        </button>
+                    </div>
+                </div>
 
             </div>
         </>
