92a18ffe36ec60e5d67cbaa8b22daff893869be1
John Doe <johndoe@example.com>
2022-01-07 22:21:40 -0800

    commiting this

--- src/component/Friendship/Friends.js
+++ src/component/Friendship/Friends.js
@@ -115,111 +115,110 @@ const [Target, setTarget] = useState("");
 
                alert("You Added Friend Successfully");
                
-//                let v;
-//                let e;
-//                let adjList;
-//                function Graph(vertices)
-// {
-//     // initialise vertex count
-//         v = vertices;
-//         initAdjList();
-//         // initialise adjacency list
-//         function initAdjList()
-// {
-//     adjList = new Array(v);
+               let v;
+               let e;
+               let adjList;
+               function Graph(vertices)
+{
+    // initialise vertex count
+        v = vertices;
+        initAdjList();
+        // initialise adjacency list
+        function initAdjList()
+{
+    adjList = new Array(v);
   
-//         for (let i = 0; i < v; i++) {
-//             adjList[i] = [];
-//         }
-//         function addEdge(u,v)
-// {
-//     // Add v to u's list.
-//     let result
-//         result = adjList[u].push(v);
-//         console.log(result);
-//         Graph(4);
-//     addEdge('sameen','sahil' );
-//     addEdge('sahil', 'adi');
-//     addEdge('sameen', 'adi');
-//     addEdge('sahil', 'sameen');
-//     addEdge('adi', 'sameen');
-//     addEdge('adi', 'sahil');
+        for (let i = 0; i < v; i++) {
+            adjList[i] = [];
+        }
+        function addEdge(u,v)
+{
+    // Add v to u's list.
+    let result
+        result = adjList[u].push(v);
+        console.log(result);
+        Graph(4);
+    addEdge('sameen','sahil' );
+    addEdge('sahil', 'adi');
+    addEdge('sameen', 'adi');
+    addEdge('sahil', 'sameen');
+    addEdge('adi', 'sameen');
+    addEdge('adi', 'sahil');
      
-//     // arbitrary source
-//     let s = SourceValue;
+    // arbitrary source
+    let s = SourceValue;
      
-//     // arbitrary destination
-//     let d = Destination;
+    // arbitrary destination
+    let d = Destination;
      
-//     console.log(
-//     "Following are all different paths from "
-//     + s + " to " + d+"<Br>");
-//     printAllPaths(s, d);
+    console.log(
+    "Following are all different paths from "
+    + s + " to " + d+"<Br>");
+    printAllPaths(s, d);
     
-// }
-// }
-// }
+}
+}
+}
             
 
 
-//         }
-//  // Prints all paths from
-//     // 's' to 'd'
-//     function printAllPaths(s,d)
-//     {
-//          let isVisited = new Array(v);
-//          for(let i=0;i<v;i++)
-//              isVisited[i]=false;
-//             let pathList = [];
+        }
+ // Prints all paths from
+    // 's' to 'd'
+    function printAllPaths(s,d)
+    {
+         let isVisited = new Array(v);
+         for(let i=0;i<v;i++)
+             isVisited[i]=false;
+            let pathList = [];
       
-//             // add source to path[]
-//             pathList.push(s);
+            // add source to path[]
+            pathList.push(s);
       
-//             // Call recursive utility
-//             printAllPathsUtil(s, d, isVisited, pathList);
-//     }
+            // Call recursive utility
+            printAllPathsUtil(s, d, isVisited, pathList);
+    }
      
-//     // A recursive function to print
-//         // all paths from 'u' to 'd'.
-//         // isVisited[] keeps track of
-//         // vertices in current path.
-//         // localPathList<> stores actual
-//         // vertices in the current path
-//     function printAllPathsUtil(u,d,isVisited,localPathList)
-//     {
-//         if (u == (d)) {
-//                 document.write(localPathList+"<br>");
-//                 // if match found then no need to 
-//                 // traverse more till depth
-//                 return;
-//             }
+    // A recursive function to print
+        // all paths from 'u' to 'd'.
+        // isVisited[] keeps track of
+        // vertices in current path.
+        // localPathList<> stores actual
+        // vertices in the current path
+    function printAllPathsUtil(u,d,isVisited,localPathList)
+    {
+        if (u == (d)) {
+                document.write(localPathList+"<br>");
+                // if match found then no need to 
+                // traverse more till depth
+                return;
+            }
       
-//             // Mark the current node
-//             isVisited[u] = true;
+            // Mark the current node
+            isVisited[u] = true;
       
-//             // Recur for all the vertices
-//             // adjacent to current vertex
-//             for (let i=0;i< adjList[u].length;i++) {
-//                 if (!isVisited[adjList[u][i]]) {
-//                     // store current node
-//                     // in path[]
-//                     localPathList.push(adjList[u][i]);
-//                     printAllPathsUtil(adjList[u][i], d, 
-//                     isVisited, localPathList);
+            // Recur for all the vertices
+            // adjacent to current vertex
+            for (let i=0;i< adjList[u].length;i++) {
+                if (!isVisited[adjList[u][i]]) {
+                    // store current node
+                    // in path[]
+                    localPathList.push(adjList[u][i]);
+                    printAllPathsUtil(adjList[u][i], d, 
+                    isVisited, localPathList);
       
-//                     // remove current node
-//                     // in path[]
-//                     localPathList.splice(localPathList.indexOf
-//                     (adjList[u][i]),1);
-//                 }
-//             }
+                    // remove current node
+                    // in path[]
+                    localPathList.splice(localPathList.indexOf
+                    (adjList[u][i]),1);
+                }
+            }
       
-//             // Mark the current node
-//             isVisited[u] = false;
-//     }
+            // Mark the current node
+            isVisited[u] = false;
+    }
      
-     // Driver program
-    // Create a sample graph
+   
     
             }
 }
